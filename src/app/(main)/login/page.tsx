6baca89f00e86ea414c8de21b32915bef4260362
mykoakbar <yeikoakbar20@gmail.com>
2025-09-30 20:57:55 +0700

    add show pass in login, add logout

--- src/app/(main)/login/page.tsx
+++ src/app/(main)/login/page.tsx
@@ -2,12 +2,15 @@
 
 import { useState } from "react"
 import { login } from "@/lib/auth"
+import { TbEye, TbEyeClosed } from "react-icons/tb"
 
 export default function LoginPage() {
   const [username, setUsername] = useState("")
   const [password, setPassword] = useState("")
   const [error, setError] = useState<string | null>(null)
   const [loading, setLoading] = useState(false)
+
+  const [ShowPassword, setShowPassword] = useState<boolean>(false);
 
   const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault()
@@ -44,19 +47,36 @@ export default function LoginPage() {
           required
         />
 
-        <input
+        {/* <input
           type="password"
           placeholder="Password"
           value={password}
           onChange={(e) => setPassword(e.target.value)}
           className="border px-3 py-2 rounded w-full"
           required
-        />
+          /> */}
+        <div className="relative w-full">
+          <input
+            value={password}
+            type={!ShowPassword ? "password" : "text"}
+            onChange={(e) => setPassword(e.target.value)}
+            placeholder="Password"
+            className={`w-full border rounded p-2`}
+            required
+          />
+          <button
+            type="button"
+            className="absolute inset-y-0 right-2 flex items-center cursor-pointer"
+            onClick={() => setShowPassword(!ShowPassword)}
+          >
+            <p className="p-1 rounded-full text-blue-600 hover:bg-blue-500 hover:text-white">{ShowPassword ? <TbEye /> : <TbEyeClosed />}</p>
+          </button>
+        </div>
 
         <button
           type="submit"
           disabled={loading}
-          className="bg-blue-600 text-white px-4 py-2 rounded hover:bg-blue-700 disabled:opacity-50"
+          className="bg-blue-600 text-white px-4 py-2 rounded hover:bg-blue-700 disabled:opacity-50 cursor-pointer"
         >
           {loading ? "Loading..." : "Login"}
         </button>
